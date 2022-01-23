65b2fff0e682f67e7ba139c7d243523d60a65abf
xi7z <92309551+xi7z@users.noreply.github.com>
2022-01-23 23:35:17 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -439,13 +439,13 @@ local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)
 local Wait = library.subs.Wait -- Only returns if the GUI has not been terminated. For 'while Wait() do' loops
 
 local PepsisWorld = library:CreateWindow({
-Name = "Pepsis's World",
+Name = "Pepsi's World",
 Themeable = {
-Info = "Discord Account: Dew#6450"
+Info = "Discord Server: VzYTJ7Y"
 }
 })
 
-local GeneralTab = DewWorld:CreateTab({
+local GeneralTab = PepsisWorld:CreateTab({
 Name = "General"
 })
 local FarmingSection = GeneralTab:CreateSection({
